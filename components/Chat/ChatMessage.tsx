c36f460827b95ff943779ef1b69d98dc26ff8a40
Alessandro Benassi <plasmax@gmail.com>
2023-04-01 22:59:42 +0200

    Update ChatMessage.tsx

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -74,8 +74,8 @@ export const ChatMessage: FC<Props> = memo(
       <div
         className={`group m-4 rounded-[5px] px-4 ${
           message.role === 'assistant'
-            ? 'border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-[#444654] dark:text-gray-100'
-            : 'border-b border-black/10 bg-white text-gray-800 dark:border-gray-900/50 dark:bg-[#343541] dark:text-gray-100'
+            ? 'border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-900 dark:text-gray-200'
+            : 'border-b border-black/10 bg-indigo-800 text-gray-800 dark:border-gray-900/50 dark:bg-indigo-800 dark:text-gray-100'
         }`}
         style={{ overflowWrap: 'anywhere' }}
       >
