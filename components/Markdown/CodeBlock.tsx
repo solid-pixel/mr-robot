dde2eca06d44625830951ed0a8baab7e8d3ff41c
Alessandro Benassi <plasmax@gmail.com>
2023-04-02 12:00:32 +0200

    commit

--- components/Markdown/CodeBlock.tsx
+++ components/Markdown/CodeBlock.tsx
@@ -6,7 +6,7 @@ import { IconCheck, IconClipboard, IconDownload } from '@tabler/icons-react';
 import { useTranslation } from 'next-i18next';
 import { FC, memo, useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface Props {
   language: string;
