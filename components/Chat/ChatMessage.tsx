2721b67f7ba77f77a70cc1a8ac449aa722d28f88
Alessandro Benassi <plasmax@gmail.com>
2023-04-10 14:51:55 +0200

    commit

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -80,8 +80,8 @@ export const ChatMessage: FC<Props> = memo(
       <div
         className={`group px-4 ${
           message.role === 'assistant'
-            ? 'border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-[#444654] dark:text-gray-100'
-            : 'border-b border-black/10 bg-white text-gray-800 dark:border-gray-900/50 dark:bg-[#343541] dark:text-gray-100'
+            ? 'solidpixel--message-ai border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-[#444654] dark:text-gray-100'
+          : 'solidpixel--message-me border-b border-black/10 bg-white text-gray-800 dark:border-gray-900/50 dark:bg-[#343541] dark:text-gray-100'
         }`}
         style={{ overflowWrap: 'anywhere' }}
       >
